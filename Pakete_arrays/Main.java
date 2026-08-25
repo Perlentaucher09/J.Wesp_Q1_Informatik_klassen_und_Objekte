426dcccdc09d738234e81af3e7d4e4e28821e905
Perlentaucher09 <wespjoshua@gmail.com>
2026-08-25 07:11:19 +0000

    Main-Klasse aktualisiert: Paket-Konstruktor um express-Attribut erweitert; Methode zur Preisberechnung hinzugefügt und Gewichtsausgabe angepasst

--- Pakete_arrays/Main.java
+++ Pakete_arrays/Main.java
@@ -5,12 +5,14 @@ public class Main {
 
         Paket [] pakete = new Paket[5];
 
-        pakete[0] = new Paket("P1001", 4.5, "Offen");
-        pakete[1] = new Paket("P1002", 12.8, "sortiert");
-        pakete[2] = new Paket("P1003", 25.4, "eingegangen");
-        pakete[3] = new Paket("P1004", 8.2, "versendet");
-        
+        pakete[0] = new Paket("P1001", 4.5, "Offen", true);
+        pakete[1] = new Paket("P1002", 12.8, "sortiert", false);
+        pakete[2] = new Paket("P1003", 25.4, "eingegangen", false);
+        pakete[3] = new Paket("P1004", 8.2, "versendet", true);
+
         gewichtausgeben(pakete);
+
+        anzeigen(pakete);
 
     }
     public static void gewichtausgeben(Paket[] pakete){
@@ -18,6 +20,15 @@ public class Main {
             if(pakete[i] != null){
                 Utility.trennung();
                 System.out.println("Gewicht: " + pakete[i].gewicht);
+            }
+        }
+        Utility.trennung();
+    }
+    public static void anzeigen(Paket[] pakete){
+        for (int i = 0; i < pakete.length; i++) {
+            if(pakete[i] != null){
+                Utility.trennung();
+                pakete[i].anzeigen();
             }
         }
         Utility.trennung();
--- Pakete_arrays/Paket.java
+++ Pakete_arrays/Paket.java
@@ -4,16 +4,40 @@ class Paket {
     String sendungsNr;
     Double gewicht;
     String status;
+    boolean express;
     
 
-    Paket(String sendungsNr, Double gewicht, String status){
+    Paket(String sendungsNr, Double gewicht, String status, boolean express){
         this.sendungsNr = sendungsNr;
         this.gewicht = gewicht;
         this.status = status;
+        this.express = express;
     }
     public void anzeigen(){
         System.out.println("Sendungsnummer: " + sendungsNr);
         System.out.println("Gewicht: " + gewicht);
         System.out.println("Status: " + status);
+        System.out.println("Express: " + express);
+        System.out.println("Preis: " + berechnePreis());
     }
+      public Double berechnePreis() {
+
+    double preis = 0.0;
+
+    if (express) {
+        preis += 5.0; // Expresszuschlag
+    }
+
+    if (gewicht <= 2.0) {
+      preis += 4.99;
+    } else if (gewicht <= 5.0) {
+      preis += 6.99;
+    } else if (gewicht <= 10.0) {
+      preis += 8.99;
+    } else {
+      preis += 12.99;
+    }
+
+    return Math.round(preis * 100.0) / 100.0; // Preis auf 2 Dezimalstellen runden
+  }
 }
