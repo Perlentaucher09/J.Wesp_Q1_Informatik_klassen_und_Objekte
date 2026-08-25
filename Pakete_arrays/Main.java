e9a8f16e52ac59470bef01a2746140143f9186a6
Perlentaucher09 <wespjoshua@gmail.com>
2026-08-25 06:44:38 +0000

    Main-Klasse aktualisiert: Lager-Array in Pakete-Array umbenannt und Referenzen angepasst

--- Pakete_arrays/Main.java
+++ Pakete_arrays/Main.java
@@ -3,16 +3,16 @@ package Pakete_arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Paket [] lager = new Paket[5];
+        Paket [] pakete = new Paket[5];
 
-        lager[0] = new Paket("P1001", 4.5, "Offen");
-        lager[1] = new Paket("P1002", 2.0, "Unterwegs");
-        lager[2] = new Paket("P1003", 3.0, "Zugestellt");
+        pakete[0] = new Paket("P1001", 4.5, "Offen");
+        pakete[1] = new Paket("P1002", 2.0, "Unterwegs");
+        pakete[2] = new Paket("P1003", 3.0, "Zugestellt");
 
-        for (int i = 0; i < lager.length; i++) {
-            if(lager[i] != null){
+        for (int i = 0; i < pakete.length; i++) {
+            if(pakete[i] != null){
                 Utility.trennung();
-                lager[i].anzeigen();
+                pakete[i].anzeigen();
                 Utility.trennung();
             }
         }
